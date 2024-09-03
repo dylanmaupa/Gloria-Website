6010d52fe99a29708f353580707b98ee2f8a841b
Dylan Maupa <dylanmaupa@gmail.com>
2024-09-03 09:48:29 +0200

    #

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import Button from '../Button'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Navigation } from 'swiper/modules';
 
 
 const HomeHero = () => {
     return (
-        <section id="hero">
-            <div className="textBox">
-                <h1>The Number <br /> One Flour for <br /> over 100 years!</h1>
-                <p>It will rise, and you will shine!</p>
-            </div>
-            <div className="btns">
-                <Button title={"Let's Bake!"} />
-                <Button title={'Contact Us'} />
-            </div>
-        </section>
+        <Swiper  navigation={true} modules={[Navigation]} className="mySwiper">
+            <SwiperSlide id="hero">
+                <div className="textBox">
+                    <h1>The Number <br /> One Flour for <br /> over 100 years!</h1>
+                    <p>It will rise, and you will shine!</p>
+                </div>
+                <div className="btns">
+                    <Button title={"Let's Bake!"} />
+                    <Button title={'Contact Us'} />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide id="hero" className='home2'>
+                <div className="textBox">
+                    <h1>The Number <br /> One Flour for <br /> over 100 years!</h1>
+                    <p>It will rise, and you will shine!</p>
+                </div>
+                <div className="btns">
+                    <Button title={"Let's Bake!"} />
+                    <Button title={'Contact Us'} />
+                </div>
+            </SwiperSlide>
+        </Swiper>
     )
 }
 
